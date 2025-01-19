367184b8013ed483b931a3c86b6503cccfe176a0
edward-burn <edburn@hotmail.co.uk>
2025-01-19 19:08:57 +0000

    some tidying

--- R/dsr.R
+++ R/dsr.R
@@ -1,23 +1,31 @@
-#' Compute Directly Standardized Rates
+#' Calculate directly standardised rates
 #'
-#' Computes crude and directly standardized rates by subgroup with confidence intervals.
+#' Computes crude and directly standardised rates. Rates can be stratified by
+#' variables of interest.
 #'
-#' @param data A data frame with counts and unit-times summarized by the standardization variables.
-#' @param event A variable within the input data that corresponds to the event counts.
-#' @param fu A variable within the input data that corresponds to the unit-time.
-#' @param subgroup A variable within the input data frame for which rates are calculated by.
-#' @param ... Variables(s) within the input data that for which rates are to be standardized by. The input data and ref data should both be summarized by these.
-#' @param refdata A data frame with population unit-times summarized by the standardization variables. The unit-time variable name must named pop.
+#' @param data A data frame with counts and unit-times summarised by the
+#' standardisation variables.
+#' @param event Name of the variable in data that corresponds to the event
+#' counts.
+#' @param time Name of the variable in data that corresponds to the unit-time.
+#' @param age Name of the variable in data that corresponds to age
+#' @param pop Name of the variable in data that corresponds to population
+#' @param strata A variable within the input data frame for which rates are
+#' calculated by.
+#' @param refdata A data frame with population unit-times summarised by the
+#' standardisation variables. The unit-time variable name must named pop.
 #' @param mp A constant to multiply rates by (e.g. mp=1000 for rates per 1000).
-#' @param method Choose between normal, lognormal and gamma confidence intervals for crude and standardized rates. The default method is normal.
-#' @param sig The desired level of confidence in computing confidence intervals. The default is 0.95 for 95 percent CIs.
+#' @param method Choose between normal, lognormal and gamma confidence intervals
+#' for crude and standardised rates. The default method is normal.
+#' @param sig The desired level of confidence in computing confidence intervals.
+#' The default is 0.95 for 95 percent CIs.
 #' @param decimals Round estimates to a desired decimal place.
-#' @references Fay, M.P., & Feuer, E.J. (1997). Confidence intervals for directly standardized rates: a method based on the gamma distribution. Statistics in Medicine,16, 791-801.
-#' @references Elandt-Johnson, R. C., and Johnson, N. L. (1980). Survival Models and Data Analysis. New York: John Wiley & Sons.
-#' @references Chiang C. Standard error of the age-adjusted death rate. US Department of Health, Education and Welfare: Vital Statistics Special Reports 1961;47:271-285.
-#' @references Schoenbach, V., and Rosamond W. (2000) Understanding the fundamentals of epidemiology: An evolving text.
+#'
+#' @importFrom rlang .data
+#' @importFrom rlang ":="
+#'
 #' @examples
-#' #An example of calculating directly standardized rates
+#' #An example of calculating directly standardised rates
 #' #Data from Table 1, Page 132 of Schoenbach (2000)
 #'
 #' #State specific death counts and fu
@@ -30,110 +38,138 @@
 #' df_ref  <- data.frame(age=c('00-14','15-24','25-44','45-64','65+'),
 #'                      pop=c(23961000,15420000,21353000,19601000,10685000))
 #'
-#' #Directly Standardized Rates (per 1000) - 95% CI's using the gamma method
-#' my_results <- dsr(data=df_study,
-#'                  event=deaths,
-#'                  fu=fu,
-#'                  subgroup=state,
-#'                  age,
-#'                  refdata=df_ref,
-#'                  method="gamma",
-#'                  sig=0.95,
-#'                  mp=1000,
-#'                  decimals=4)
+#' #Directly standardised Rates (per 1000) - 95% CI's using the gamma method
+#' my_results <- dsr(data = df_study,
+#'                   event = "deaths",
+#'                   time = "fu",
+#'                   strata = "state",
+#'                   age = "age",
+#'                   refdata = df_ref)
 #' #View results
 #' my_results
 #' @export
 
-dsr <- function(data, event, fu, subgroup, ..., refdata, mp, method="normal", sig=0.95, decimals ) {
+dsr <- function(data,
+                event,
+                time,
+                age = "age_group",
+                pop = "pop",
+                strata = NULL,
+                refdata  = standardPopulation("esp2013"),
+                mp = 1000,
+                method = "normal",
+                sig = 0.95,
+                decimals = 4) {
 
-  subgroup <- rlang::enquo(subgroup)
-  event <- rlang::enquo(event)
-  fu <- rlang::enquo(fu)
-  stdvars <- rlang::quos(...)
-
-
-
-  #Compute crude and standardized rates and variances
   all_data_st <- data |>
-    dplyr::left_join(refdata,
-                     by = dplyr::join_by("age")) |>
-    dplyr::group_by(!!subgroup) |>
-    dplyr::mutate(n=sum(!!event),
-           d=sum(!!fu),
-           cr_rate=.data$n/.data$d,
-           cr_var=.data$n/.data$d^2,
-           wts=.data$pop/sum(.data$pop),
-           st_rate=sum(.data$wts*(!!event/!!fu)),
-           st_var=sum(as.numeric((.data$wts^2)*(!!event/(!!fu )^2)))) |>
-    dplyr::distinct(!!subgroup, .keep_all=TRUE) |>
-    dplyr::select(!!subgroup, "n", "d", "cr_rate", "cr_var", "st_rate", "st_var")
+    dplyr::left_join(refdata, by = dplyr::join_by(!!rlang::sym(age)))
 
+  if (!is.null(strata)) {
+    all_data_st <- all_data_st |>
+      dplyr::group_by(!!!rlang::syms(strata))
+  }
+  all_data_st <- all_data_st |>
+    dplyr::mutate(n = sum(!!rlang::sym(event)),
+                  d = sum(!!rlang::sym(time))) |>
+    dplyr::mutate(
+      cr_rate = .data$n / .data$d,
+      cr_var = .data$n / .data$d ^ 2,
+      wts = .data$pop / sum(.data$pop),
+      st_rate = sum(.data$wts * (!!rlang::sym(event) / !!rlang::sym(time))),
+      st_var = sum(as.numeric((.data$wts ^ 2) * (
+        !!rlang::sym(event) / (!!rlang::sym(time)) ^ 2
+      )))
+    ) |>
+    dplyr::distinct(!!!strata, .keep_all = TRUE) |>
+    dplyr::select(dplyr::all_of(c(
+      strata, "n", "d", "cr_rate", "cr_var", "st_rate", "st_var"
+    )))
 
-
-  #Compute Confidence Intervals (CI) according to method. The default is 'normal'
-  if(method=="gamma") {
-
-    tmp1 <- all_data_st |>
-      dplyr::mutate(
-        c_rate=mp*.data$cr_rate,
-        c_lower=mp*stats::qgamma((1-sig)/2, shape=.data$cr_rate^2/(.data$cr_var))/(.data$cr_rate/.data$cr_var),
-        c_upper=mp*stats::qgamma(1-((1-sig)/2), shape=1+.data$cr_rate^2/(.data$cr_var))/(.data$cr_rate/.data$cr_var),
-        s_rate=mp*.data$st_rate,
-        s_lower=mp*stats::qgamma((1-sig)/2, shape=.data$st_rate^2/.data$st_var)/(.data$st_rate/.data$st_var),
-        s_upper=mp*stats::qgamma(1-((1-sig)/2), shape=1+(.data$st_rate^2/.data$st_var))/(.data$st_rate/.data$st_var)) |>
-      dplyr::select(!!subgroup, "n", "d", "c_rate", "c_lower", "c_upper", "s_rate", "s_lower", "s_upper")
-
-
-  } else if (method=="normal") {
-
-
-    tmp1 <- all_data_st |>
-      dplyr::mutate(
-        c_rate=mp*.data$cr_rate,
-        c_lower=mp*(.data$cr_rate+stats::qnorm((1-sig)/2)*sqrt(.data$cr_var)),
-        c_upper=mp*(.data$cr_rate-stats::qnorm((1-sig)/2)*sqrt(.data$cr_var)),
-        s_rate=mp*.data$st_rate,
-        s_lower=mp*(.data$st_rate+stats::qnorm((1-sig)/2)*sqrt(.data$st_var)),
-        s_upper=mp*(.data$st_rate-stats::qnorm((1-sig)/2)*sqrt(.data$st_var))) |>
-      dplyr::select(!!subgroup, "n", "d", "c_rate", "c_lower", "c_upper", "s_rate", "s_lower", "s_upper")
-
-  } else if (method=="lognormal") {
-
-
-    tmp1 <- all_data_st |>
-      dplyr::mutate(
-        c_rate=mp*.data$cr_rate,
-        c_lower=mp*exp((log(.data$cr_rate)+stats::qnorm((1-sig)/2)*sqrt(.data$cr_var)/(.data$cr_rate))),
-        c_upper=mp*exp((log(.data$cr_rate)-stats::qnorm((1-sig)/2)*sqrt(.data$cr_var)/(.data$cr_rate))),
-        s_rate=mp*.data$st_rate,
-        s_lower=mp*exp((log(.data$st_rate)+stats::qnorm((1-sig)/2)*sqrt(.data$st_var)/(.data$st_rate))),
-        s_upper=mp*exp((log(.data$st_rate)-stats::qnorm((1-sig)/2)*sqrt(.data$st_var)/(.data$st_rate)))) |>
-      dplyr::select(!!subgroup, "n", "d", "c_rate", "c_lower", "c_upper", "s_rate", "s_lower", "s_upper")
-
+  if (!is.null(strata)) {
+    all_data_st <- all_data_st |>
+      dplyr::ungroup()
   }
 
+  # Compute Confidence Intervals (CI) according to method. The default is 'normal'
+  if (method == "gamma") {
+    tmp1 <- all_data_st |>
+      dplyr::mutate(
+        c_rate = mp * .data$cr_rate,
+        c_lower = mp * stats::qgamma((1 - sig) / 2, shape = .data$cr_rate ^
+                                       2 / (.data$cr_var)) / (.data$cr_rate / .data$cr_var),
+        c_upper = mp * stats::qgamma(1 - ((1 - sig) / 2), shape = 1 + .data$cr_rate ^
+                                       2 / (.data$cr_var)) / (.data$cr_rate / .data$cr_var),
+        s_rate = mp * .data$st_rate,
+        s_lower = mp * stats::qgamma((1 - sig) / 2, shape = .data$st_rate ^
+                                       2 / .data$st_var) / (.data$st_rate / .data$st_var),
+        s_upper = mp * stats::qgamma(1 - ((1 - sig) / 2), shape = 1 + (.data$st_rate ^
+                                                                         2 / .data$st_var)) / (.data$st_rate / .data$st_var)
+      )
+
+  } else if (method == "normal") {
+    tmp1 <- all_data_st |>
+      dplyr::mutate(
+        c_rate = mp * .data$cr_rate,
+        c_lower = mp * (.data$cr_rate + stats::qnorm((1 - sig) / 2) * sqrt(.data$cr_var)),
+        c_upper = mp * (.data$cr_rate - stats::qnorm((1 - sig) / 2) * sqrt(.data$cr_var)),
+        s_rate = mp * .data$st_rate,
+        s_lower = mp * (.data$st_rate + stats::qnorm((1 - sig) / 2) * sqrt(.data$st_var)),
+        s_upper = mp * (.data$st_rate - stats::qnorm((1 - sig) / 2) * sqrt(.data$st_var))
+      )
+
+  } else if (method == "lognormal") {
+    tmp1 <- all_data_st |>
+      dplyr::mutate(
+        c_rate = mp * .data$cr_rate,
+        c_lower = mp * exp((
+          log(.data$cr_rate) + stats::qnorm((1 - sig) / 2) * sqrt(.data$cr_var) /
+            (.data$cr_rate)
+        )),
+        c_upper = mp * exp((
+          log(.data$cr_rate) - stats::qnorm((1 - sig) / 2) * sqrt(.data$cr_var) /
+            (.data$cr_rate)
+        )),
+        s_rate = mp * .data$st_rate,
+        s_lower = mp * exp((
+          log(.data$st_rate) + stats::qnorm((1 - sig) / 2) * sqrt(.data$st_var) /
+            (.data$st_rate)
+        )),
+        s_upper = mp * exp((
+          log(.data$st_rate) - stats::qnorm((1 - sig) / 2) * sqrt(.data$st_var) /
+            (.data$st_rate)
+        ))
+      )
+  }
 
   #Clean up and output
+  tmp1$c_rate  <- round(tmp1$c_rate, digits = decimals)
+  tmp1$c_lower <- round(tmp1$c_lower, digits = decimals)
+  tmp1$c_upper <- round(tmp1$c_upper, digits = decimals)
+  tmp1$s_rate  <- round(tmp1$s_rate, digits = decimals)
+  tmp1$s_lower <- round(tmp1$s_lower, digits = decimals)
+  tmp1$s_upper <- round(tmp1$s_upper, digits = decimals)
 
-  tmp1$c_rate  <- round(tmp1$c_rate,  digits=decimals)
-  tmp1$c_lower <- round(tmp1$c_lower, digits=decimals)
-  tmp1$c_upper <- round(tmp1$c_upper, digits=decimals)
-  tmp1$s_rate  <- round(tmp1$s_rate, digits=decimals)
-  tmp1$s_lower <- round(tmp1$s_lower, digits=decimals)
-  tmp1$s_upper <- round(tmp1$s_upper, digits=decimals)
+  c_rate_name <- paste0('Crude Rate (per ', mp, ')')
+  c_lower_name <- paste0(sig * 100, '% LCL (Crude)')
+  c_upper_name <- paste0(sig * 100, '% UCL (Crude)')
+  s_rate_name <- paste0('Std Rate (per ', mp, ')')
+  s_lower_name <- paste0(sig * 100, '% LCL (Std)')
+  s_upper_name <- paste0(sig * 100, '% UCL (Std)')
 
-  colnames(tmp1) <-  c('Subgroup', 'Numerator','Denominator',
-                       paste('Crude Rate (per ',mp,')',sep=''),
-                       paste(sig*100,'% LCL (Crude)',sep=''),
-                       paste(sig*100,'% UCL (Crude)',sep=''),
-                       paste('Std Rate (per ',mp,')',sep=''),
-                       paste(sig*100,'% LCL (Std)',sep=''),
-                       paste(sig*100,'% UCL (Std)',sep=''))
-
-
-  tmp1 <- as.data.frame(tmp1)
+  tmp1 <- tmp1 |>
+    dplyr::select(
+      strata,
+      "Numerator" = "n",
+      "Denominator" = "d",
+      !!c_rate_name := "c_rate",
+      !!c_lower_name := "c_lower",
+      !!c_upper_name := "c_upper",
+      !!s_rate_name := "s_rate",
+      !!s_lower_name := "s_lower",
+      !!s_upper_name := "s_upper")
 
   tmp1
 
 }
+
+
--- tests/testthat/test-dsr.R
+++ tests/testthat/test-dsr.R
@@ -1,30 +1,94 @@
 test_that("dsr example", {
   # example from original dsr package
-  #An example of calculating directly standardized rates
-  #Data from Table 1, Page 132 of Schoenbach (2000)
+  # An example of calculating directly standardized rates
+  # Data from Table 1, Page 132 of Schoenbach (2000)
 
-  #State specific death counts and fu
+  # State specific death counts and fu
   df_study <- data.frame(state=rep(c('Miami',"Alaska"), c(5,5)),
                         age=rep(c('00-14','15-24','25-44','45-64','65+'),2),
                         deaths=c(136,57,208,1016,3605,59,18,37,90,81),
                         fu=c(114350,80259,133440,142670,92168,37164,20036,32693,14947,2077))
 
-  #US standard population
+  # US standard population
   df_ref  <- data.frame(age=c('00-14','15-24','25-44','45-64','65+'),
                        pop=c(23961000,15420000,21353000,19601000,10685000))
 
-  #Directly Standardized Rates (per 1000) - 95% CI's using the gamma method
+  # Directly Standardized Rates (per 1000) - 95% CI's using the gamma method
   expect_no_error(my_results <- dsr(data=df_study,
-                   event=deaths,
-                   fu=fu,
-                   subgroup=state,
-                   age,
+                   event="deaths",
+                   time="fu",
+                   strata="state",
+                   age = "age",
                    refdata=df_ref,
                    method="gamma",
                    sig=0.95,
                    mp=1000,
                    decimals=4))
+  expect_true(inherits(my_results, "data.frame"))
 
   # ADD EXPECTED VALUES
+
+})
+
+test_that("using package populations", {
+
+  # european
+  stud_result <- data.frame(
+    age_group = c(
+      "0-4 years", "5-9 years", "10-14 years",
+      "15-19 years", "20-24 years", "25-29 years",
+      "30-34 years", "35-39 years", "40-44 years",
+      "45-49 years", "50-54 years", "55-59 years",
+      "60-64 years", "65-69 years", "70-74 years",
+      "75-79 years","80-84 years","85-89 years",
+      "90plus years"
+    ),
+    deaths= rep(5, 19),
+    time = rep(100, 19))
+  expect_no_error(dsr(data = stud_result,
+      event = "deaths", time = "time",
+      refdata  = standardPopulation("esp2013")))
+
+  stud_result <- data.frame(
+    var_1 = c(rep("a", 19),
+              rep("b", 19)),
+    age_group = rep(c(
+      "0-4 years", "5-9 years", "10-14 years",
+      "15-19 years", "20-24 years", "25-29 years",
+      "30-34 years", "35-39 years", "40-44 years",
+      "45-49 years", "50-54 years", "55-59 years",
+      "60-64 years", "65-69 years", "70-74 years",
+      "75-79 years","80-84 years","85-89 years",
+      "90plus years"
+    ),2),
+    deaths= c(rep(5, 19),rep(10, 19)),
+    time = rep(100, 38))
+  expect_no_error(dsr(data = stud_result,
+                      event = "deaths", time = "time",
+                      strata = "var_1",
+                      refdata  = standardPopulation("esp2013")))
+  stud_result <- data.frame(
+    var_1 = c(rep("a", 19),
+              rep("b", 19)),
+    var_2 = rep(c("c","d"), 19),
+    age_group = rep(c(
+      "0-4 years", "5-9 years", "10-14 years",
+      "15-19 years", "20-24 years", "25-29 years",
+      "30-34 years", "35-39 years", "40-44 years",
+      "45-49 years", "50-54 years", "55-59 years",
+      "60-64 years", "65-69 years", "70-74 years",
+      "75-79 years","80-84 years","85-89 years",
+      "90plus years"
+    ),2),
+    deaths= c(rep(5, 19),rep(10, 19)),
+    time = rep(100, 38))
+  expect_no_error(dsr(data = stud_result,
+                      event = "deaths", time = "time",
+                      strata = c("var_1", "var_2"),
+                      refdata  = standardPopulation("esp2013")))
+
+
+
+  # world
 
 })
