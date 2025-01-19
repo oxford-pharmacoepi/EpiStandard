09ce36d5895534c0edf28957d4827e0010d45061
edward-burn <edburn@hotmail.co.uk>
2025-01-19 11:56:51 +0000

    render readme, test, output df

--- R/dsr.R
+++ R/dsr.R
@@ -16,10 +16,6 @@
 #' @references Elandt-Johnson, R. C., and Johnson, N. L. (1980). Survival Models and Data Analysis. New York: John Wiley & Sons.
 #' @references Chiang C. Standard error of the age-adjusted death rate. US Department of Health, Education and Welfare: Vital Statistics Special Reports 1961;47:271-285.
 #' @references Schoenbach, V., and Rosamond W. (2000) Understanding the fundamentals of epidemiology: An evolving text.
-#' @import stats
-#' @import dplyr
-#' @import rlang
-#' @import utils
 #' @examples
 #' #An example of calculating directly standardized rates
 #' #Data from Table 1, Page 132 of Schoenbach (2000)
@@ -51,68 +47,69 @@
 
 dsr <- function(data, event, fu, subgroup, ..., refdata, mp, method="normal", sig=0.95, decimals ) {
 
-  subgroup <- enquo(subgroup)
-  event <- enquo(event)
-  fu <- enquo(fu)
-  stdvars <- quos(...)
+  subgroup <- rlang::enquo(subgroup)
+  event <- rlang::enquo(event)
+  fu <- rlang::enquo(fu)
+  stdvars <- rlang::quos(...)
 
 
 
   #Compute crude and standardized rates and variances
-  all_data_st = data %>%
-    left_join(refdata) %>%
-    group_by(!!subgroup) %>%
-    mutate(n=sum(!!event),
+  all_data_st <- data |>
+    dplyr::left_join(refdata,
+                     by = dplyr::join_by("age")) |>
+    dplyr::group_by(!!subgroup) |>
+    dplyr::mutate(n=sum(!!event),
            d=sum(!!fu),
-           cr_rate=n/d,
-           cr_var=n/d^2,
-           wts=pop/sum(pop),
-           st_rate=sum(wts*(!!event/!!fu)),
-           st_var=sum(as.numeric((wts^2)*(!!event/(!!fu )^2)))) %>%
-    distinct(!!subgroup, .keep_all=TRUE) %>%
-    select(!!subgroup, n, d, cr_rate, cr_var, st_rate, st_var)
+           cr_rate=.data$n/.data$d,
+           cr_var=.data$n/.data$d^2,
+           wts=.data$pop/sum(.data$pop),
+           st_rate=sum(.data$wts*(!!event/!!fu)),
+           st_var=sum(as.numeric((.data$wts^2)*(!!event/(!!fu )^2)))) |>
+    dplyr::distinct(!!subgroup, .keep_all=TRUE) |>
+    dplyr::select(!!subgroup, "n", "d", "cr_rate", "cr_var", "st_rate", "st_var")
 
 
 
   #Compute Confidence Intervals (CI) according to method. The default is 'normal'
   if(method=="gamma") {
 
-    tmp1 =   all_data_st %>%
-      mutate(
-        c_rate=mp*cr_rate,
-        c_lower=mp*qgamma((1-sig)/2, shape=cr_rate^2/(cr_var))/(cr_rate/cr_var),
-        c_upper=mp*qgamma(1-((1-sig)/2), shape=1+cr_rate^2/(cr_var))/(cr_rate/cr_var),
-        s_rate=mp*st_rate,
-        s_lower=mp*qgamma((1-sig)/2, shape=st_rate^2/st_var)/(st_rate/st_var),
-        s_upper=mp*qgamma(1-((1-sig)/2), shape=1+(st_rate^2/st_var))/(st_rate/st_var)) %>%
-      select(!!subgroup, n, d, c_rate, c_lower, c_upper, s_rate, s_lower, s_upper)
+    tmp1 <- all_data_st |>
+      dplyr::mutate(
+        c_rate=mp*.data$cr_rate,
+        c_lower=mp*stats::qgamma((1-sig)/2, shape=.data$cr_rate^2/(.data$cr_var))/(.data$cr_rate/.data$cr_var),
+        c_upper=mp*stats::qgamma(1-((1-sig)/2), shape=1+.data$cr_rate^2/(.data$cr_var))/(.data$cr_rate/.data$cr_var),
+        s_rate=mp*.data$st_rate,
+        s_lower=mp*stats::qgamma((1-sig)/2, shape=.data$st_rate^2/.data$st_var)/(.data$st_rate/.data$st_var),
+        s_upper=mp*stats::qgamma(1-((1-sig)/2), shape=1+(.data$st_rate^2/.data$st_var))/(.data$st_rate/.data$st_var)) |>
+      dplyr::select(!!subgroup, "n", "d", "c_rate", "c_lower", "c_upper", "s_rate", "s_lower", "s_upper")
 
 
   } else if (method=="normal") {
 
 
-    tmp1 =   all_data_st %>%
-      mutate(
-        c_rate=mp*cr_rate,
-        c_lower=mp*(cr_rate+qnorm((1-sig)/2)*sqrt(cr_var)),
-        c_upper=mp*(cr_rate-qnorm((1-sig)/2)*sqrt(cr_var)),
-        s_rate=mp*st_rate,
-        s_lower=mp*(st_rate+qnorm((1-sig)/2)*sqrt(st_var)),
-        s_upper=mp*(st_rate-qnorm((1-sig)/2)*sqrt(st_var))) %>%
-      select(!!subgroup, n, d, c_rate, c_lower, c_upper, s_rate, s_lower, s_upper)
+    tmp1 <- all_data_st |>
+      dplyr::mutate(
+        c_rate=mp*.data$cr_rate,
+        c_lower=mp*(.data$cr_rate+stats::qnorm((1-sig)/2)*sqrt(.data$cr_var)),
+        c_upper=mp*(.data$cr_rate-stats::qnorm((1-sig)/2)*sqrt(.data$cr_var)),
+        s_rate=mp*.data$st_rate,
+        s_lower=mp*(.data$st_rate+stats::qnorm((1-sig)/2)*sqrt(.data$st_var)),
+        s_upper=mp*(.data$st_rate-stats::qnorm((1-sig)/2)*sqrt(.data$st_var))) |>
+      dplyr::select(!!subgroup, "n", "d", "c_rate", "c_lower", "c_upper", "s_rate", "s_lower", "s_upper")
 
   } else if (method=="lognormal") {
 
 
-    tmp1 =   all_data_st %>%
-      mutate(
-        c_rate=mp*cr_rate,
-        c_lower=mp*exp((log(cr_rate)+qnorm((1-sig)/2)*sqrt(cr_var)/(cr_rate))),
-        c_upper=mp*exp((log(cr_rate)-qnorm((1-sig)/2)*sqrt(cr_var)/(cr_rate))),
-        s_rate=mp*st_rate,
-        s_lower=mp*exp((log(st_rate)+qnorm((1-sig)/2)*sqrt(st_var)/(st_rate))),
-        s_upper=mp*exp((log(st_rate)-qnorm((1-sig)/2)*sqrt(st_var)/(st_rate)))) %>%
-      select(!!subgroup, n, d, c_rate, c_lower, c_upper, s_rate, s_lower, s_upper)
+    tmp1 <- all_data_st |>
+      dplyr::mutate(
+        c_rate=mp*.data$cr_rate,
+        c_lower=mp*exp((log(.data$cr_rate)+stats::qnorm((1-sig)/2)*sqrt(.data$cr_var)/(.data$cr_rate))),
+        c_upper=mp*exp((log(.data$cr_rate)-stats::qnorm((1-sig)/2)*sqrt(.data$cr_var)/(.data$cr_rate))),
+        s_rate=mp*.data$st_rate,
+        s_lower=mp*exp((log(.data$st_rate)+stats::qnorm((1-sig)/2)*sqrt(.data$st_var)/(.data$st_rate))),
+        s_upper=mp*exp((log(.data$st_rate)-stats::qnorm((1-sig)/2)*sqrt(.data$st_var)/(.data$st_rate)))) |>
+      dplyr::select(!!subgroup, "n", "d", "c_rate", "c_lower", "c_upper", "s_rate", "s_lower", "s_upper")
 
   }
 
@@ -136,5 +133,7 @@ dsr <- function(data, event, fu, subgroup, ..., refdata, mp, method="normal", si
 
 
   tmp1 <- as.data.frame(tmp1)
+
+  tmp1
 
 }
