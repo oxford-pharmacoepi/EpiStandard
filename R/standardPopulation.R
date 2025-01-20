8ddde3041a5e8c8f3d009410a74518b275dbab7d
Elin Rowlands <elinrow@gmail.com>
2025-01-20 11:32:13 +0000

    Add new databases as options.

--- R/standardPopulation.R
+++ R/standardPopulation.R
@@ -1,7 +1,7 @@
 
 #' Standard population
 #'
-#' @param name Dataset to use. One of "esp2013" or ""wsp2025".
+#' @param name Dataset to use. One of "esp2013", "wsp2025", "esp2013_by_sex" or "wsp2025_by_sex".
 #'
 #' @return Tibble
 #' @export
@@ -10,7 +10,7 @@
 #' standardPopulation()
 standardPopulation <- function(name = "esp2013"){
 
-  supported_names <- c("esp2013", "wsp2025")
+  supported_names <- c("esp2013", "wsp2025", "esp2013_by_sex", "wsp2025_by_sex")
   if(!name %in% supported_names){
     cli::cli_abort("{name} not available - name must be one of: {supported_names}")
   }
@@ -21,6 +21,14 @@ standardPopulation <- function(name = "esp2013"){
 
   if(name == "wsp2025"){
     return(get("wsp2025"))
+  }
+
+  if(name == "esp2013_by_sex"){
+    return(get("esp2013_by_sex"))
+  }
+
+  if(name == "wsp2025_by_sex"){
+    return(get("wsp2025_by_sex"))
   }
 
 }
--- tests/testthat/test-standardPopulation.R
+++ tests/testthat/test-standardPopulation.R
@@ -3,6 +3,8 @@ test_that("load standard population", {
   expect_true(inherits(standardPopulation(), "data.frame"))
   expect_true(inherits(standardPopulation("esp2013"), "data.frame"))
   expect_true(inherits(standardPopulation("wsp2025"), "data.frame"))
+  expect_true(inherits(standardPopulation("esp2013_by_sex"), "data.frame"))
+  expect_true(inherits(standardPopulation("wsp2025_by_sex"), "data.frame"))
 
   # unsupported datase
   expect_error(standardPopulation(name = "another"))
