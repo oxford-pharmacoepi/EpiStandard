edae7d22a7ce6d5f4fd51d7977b019b304715d1b
Elin Rowlands <elinrow@gmail.com>
2025-02-10 17:20:56 +0000

    Add new function 'ageGroups()'.

---
+++ R/ageGroups.R
@@ -0,0 +1,67 @@
+#' Age groups
+#'
+#' @param name Dataset - One of "esp2013" or ""wsp2025".
+#'
+#' @return List
+#' @export
+#'
+#' @examples
+#' ageGroups(name = "esp2013")
+#' @export
+#'
+
+ageGroups <- function(name = "esp2013"){
+
+  supported_names <- c("esp2013", "wsp2025")
+  if(!name %in% supported_names){
+    cli::cli_abort("{name} not available - name must be one of: {supported_names}")
+  }
+
+  if(name == "esp2013"){
+    return(list(
+      c(0,4),
+      c(5,9),
+      c(10,14),
+      c(15,19),
+      c(20,24),
+      c(25,29),
+      c(30,34),
+      c(35,39),
+      c(40,44),
+      c(45,49),
+      c(50,54),
+      c(55,59),
+      c(60,64),
+      c(65,69),
+      c(70,74),
+      c(75,79),
+      c(80,84),
+      c(85,89),
+      c(90,150)
+    ))
+  }
+
+  if(name == "wsp2025"){
+    return(list(
+      c(0,4),
+      c(5,9),
+      c(10,14),
+      c(15,19),
+      c(20,24),
+      c(25,29),
+      c(30,34),
+      c(35,39),
+      c(40,44),
+      c(45,49),
+      c(50,54),
+      c(55,59),
+      c(60,64),
+      c(65,69),
+      c(70,74),
+      c(75,79),
+      c(80,84),
+      c(85,150)
+    ))
+  }
+
+}
