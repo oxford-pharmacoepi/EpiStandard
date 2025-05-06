d2f2334e6d7d76999742e754b3f159b5e880e861
Elin Rowlands <elinrow@gmail.com>
2025-05-06 15:15:39 +0100

    Correct test.

--- tests/testthat/test-ageGroups.R
+++ tests/testthat/test-ageGroups.R
@@ -1,8 +1,8 @@
 test_that("load age groups", {
 
-  expect_true(inherits(ageGroups(), "list"))
-  expect_true(inherits(ageGroups("esp2013"), "list"))
-  expect_true(inherits(ageGroups("wsp2025"), "list"))
+  expect_true(inherits(ageGroups(), "character"))
+  expect_true(inherits(ageGroups("esp2013"), "character"))
+  expect_true(inherits(ageGroups("wsp2025"), "character"))
 
   # unsupported datase
   expect_error(ageGroups(name = "another"))
